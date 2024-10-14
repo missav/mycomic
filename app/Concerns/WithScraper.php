27979b60b8313a6017572c64404874dfc7e783e3
Eric Chow <yateric@gmail.com>
2024-10-14 11:35:33 +0100

    increase retry

--- app/Concerns/WithScraper.php
+++ app/Concerns/WithScraper.php
@@ -10,7 +10,7 @@ trait WithScraper
 {
     public function scrap(string $url): string
     {
-        return retry(5, fn () => app(Scrapfly::class)->scrap($url)['result']['content'], 300, fn (Exception $e) =>
+        return retry(10, fn () => app(Scrapfly::class)->scrap($url)['result']['content'], 300, fn (Exception $e) =>
             ! ($e instanceof ScrapflyRequestException && $e->getCode() === 404)
         );
     }
