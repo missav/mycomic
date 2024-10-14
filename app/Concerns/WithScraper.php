ead1928dbdc4ce13119dad41ec932b16e191281c
Eric Chow <yateric@gmail.com>
2024-10-14 21:49:56 +0100

    use laravel http for scraper

--- app/Concerns/WithScraper.php
+++ app/Concerns/WithScraper.php
@@ -2,16 +2,17 @@
 
 namespace App\Concerns;
 
-use App\Scrapfly\Scrapfly;
-use App\Scrapfly\ScrapflyRequestException;
 use Exception;
+use Illuminate\Support\Facades\Http;
 
 trait WithScraper
 {
     public function scrap(string $url): string
     {
-        return retry(10, fn () => app(Scrapfly::class)->scrap($url)['result']['content'], 300, fn (Exception $e) =>
-            ! ($e instanceof ScrapflyRequestException && $e->getCode() === 404)
-        );
+        return Http::proxy()
+            ->retry(10, 500, fn (Exception $e) => $e->getCode() !== 404)
+            ->get($url)
+            ->throw()
+            ->body();
     }
 }
