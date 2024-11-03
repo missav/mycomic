3452352f11ec8bb607dc3937f5d65183991ef11e
Eric Chow <yateric@gmail.com>
2024-11-03 21:19:13 +0000

    update layout

--- resources/views/components/comic-section.blade.php
+++ resources/views/components/comic-section.blade.php
@@ -14,10 +14,10 @@
         @endif
     </div>
 @endif
-<div {{ $attributes->merge(['class' => 'grid grid-cols-2 gap-x-6 gap-y-10 sm:grid-cols-3 lg:grid-cols-6 xl:gap-x-8']) }}>
+<div {{ $attributes->merge(['class' => 'grid grid-cols-3 lg:grid-cols-6 gap-x-2 lg:gap-x-4 xl:gap-x-6 gap-y-6 lg:gap-y-8']) }}>
     <template x-for="comic in comics">
         <div class="group relative">
-            <div class="aspect-w-5 aspect-h-3 sm:aspect-w-1 sm:aspect-h-1 lg:aspect-w-3 lg:aspect-h-4 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75">
+            <div class="aspect-w-3 aspect-h-4 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75">
                 <a :href="comic.id ? comicUrl(comic) : '#'" wire:navigate>
                     <img
                         @if ($lozad)
