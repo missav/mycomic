68ae0cd661ea2ba19379de9259fff30832606583
Eric Chow <yateric@gmail.com>
2024-11-03 22:43:15 +0000

    update strings

--- lang/cn/auth.php
+++ lang/cn/auth.php
@@ -2,6 +2,4 @@
 
 return [
     'failed' => '密码不正确。',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 ];
--- lang/zh/auth.php
+++ lang/zh/auth.php
@@ -2,6 +2,4 @@
 
 return [
     'failed' => '密碼不正確。',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 ];
