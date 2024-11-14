0888dd15c49d8063f20d7006cf2d8e9992e796d1
Eric Chow <yateric@gmail.com>
2024-11-14 13:24:20 +0000

    update temp command

--- app/Console/Commands/TempCommand.php
+++ app/Console/Commands/TempCommand.php
@@ -2,7 +2,9 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Comic;
 use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Collection;
 
 class TempCommand extends Command
 {
@@ -12,5 +14,20 @@ class TempCommand extends Command
 
     public function handle(): void
     {
+        Comic::query()
+            ->whereNull('recent_chapter_id')
+            ->chunkById(1000, function (Collection $comics) {
+                $comics->each(function (Comic $comic) {
+                    if ($recentChapter = $comic->recentChapter) {
+                        $comic->update([
+                            'recent_chapter_id' => $recentChapter->id,
+                            'recent_chapter_title' => $recentChapter->title,
+                        ]);
+                    }
+                    $this->info("Updated {$comic->id}");
+                });
+            });
+
+        $this->info('Done');
     }
 }
