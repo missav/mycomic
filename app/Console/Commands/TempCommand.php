cf0197e42937c488813b9d4b3be7bb31d9b60e57
Eric Chow <yateric@gmail.com>
2024-11-17 00:06:54 +0000

    update temp command

--- app/Console/Commands/TempCommand.php
+++ app/Console/Commands/TempCommand.php
@@ -2,6 +2,8 @@
 
 namespace App\Console\Commands;
 
+use App\Jobs\FetchChapterPageSizes;
+use App\Models\Chapter;
 use Illuminate\Console\Command;
 
 class TempCommand extends Command
@@ -12,5 +14,14 @@ class TempCommand extends Command
 
     public function handle(): void
     {
+        Chapter::query()
+            ->whereNull('page_sizes')
+            ->limit(100)
+            ->get()
+            ->each(function (Chapter $chapter) {
+                FetchChapterPageSizes::dispatchSync($chapter);
+
+                $this->info("Queued chapter #{$chapter->id}");
+            });
     }
 }
