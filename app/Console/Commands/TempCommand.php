3539e5e92fad16a23586bd77668dafa681acbf50
Eric Chow <yateric@gmail.com>
2024-11-18 22:30:59 +0000

    cleanup

--- app/Console/Commands/TempCommand.php
+++ app/Console/Commands/TempCommand.php
@@ -2,10 +2,7 @@
 
 namespace App\Console\Commands;
 
-use App\Jobs\FetchChapterPageSizes;
-use App\Models\Chapter;
 use Illuminate\Console\Command;
-use Illuminate\Database\Eloquent\Collection;
 
 class TempCommand extends Command
 {
@@ -15,16 +12,5 @@ class TempCommand extends Command
 
     public function handle(): void
     {
-        Chapter::query()
-            ->whereNull('page_sizes')
-            ->chunkById(1000, function (Collection $chapters) {
-                $chapters->each(function (Chapter $chapter) {
-                    FetchChapterPageSizes::dispatch($chapter);
-
-                    $this->info("Queued chapter #{$chapter->id}");
-                });
-            });
-
-        $this->info('Queued all chapter');
     }
 }
