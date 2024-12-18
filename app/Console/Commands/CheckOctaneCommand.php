aa81b32c8df5d042ab62d8ea71bf884d3fa0be01
Eric Chow <yateric@gmail.com>
2024-12-18 07:21:49 +0000

    switch check octane to one off command

--- app/Console/Commands/CheckOctaneCommand.php
+++ app/Console/Commands/CheckOctaneCommand.php
@@ -18,40 +18,36 @@ class CheckOctaneCommand extends Command
 
     public function handle(): void
     {
-        while (true) {
-            $isRunning = match (config('octane.server')) {
-                'swoole' => $this->isSwooleServerRunning(),
-                'roadrunner' => $this->isRoadRunnerServerRunning(),
-                'frankenphp' => $this->isFrankenPhpServerRunning(),
-            };
-            if ($isRunning) {
-                return;
-            }
-
-            $ploi = app(Ploi::class);
-            $server = app(Ploi::class)->servers()->firstWhere('name', gethostname());
-            if (! $server) {
-                return;
-            }
-
-            $daemon = collect($ploi->api("servers/{$server['id']}/daemons"))->first(fn (array $daemon) =>
-            Str::contains($daemon['command'], 'octane:start')
-            );
-            if (! $daemon) {
-                return;
-            }
-
-            $port = 8000;
-            Shell::exec("fuser -n tcp -k {$port}");
-            $this->info("Kill processes consuming port {$port}");
-
-            sleep(3);
-
-            $ploi->api("servers/{$server['id']}/daemons/{$daemon['id']}/restart", method: 'POST');
-            $this->info('Restarted Laravel Octane daemon');
-
-            sleep(3);
+        $isRunning = match (config('octane.server')) {
+            'swoole' => $this->isSwooleServerRunning(),
+            'roadrunner' => $this->isRoadRunnerServerRunning(),
+            'frankenphp' => $this->isFrankenPhpServerRunning(),
+        };
+        if ($isRunning) {
+            return;
         }
+
+        $ploi = app(Ploi::class);
+        $server = app(Ploi::class)->servers()->firstWhere('name', gethostname());
+        if (! $server) {
+            return;
+        }
+
+        $daemon = collect($ploi->api("servers/{$server['id']}/daemons"))->first(fn (array $daemon) =>
+        Str::contains($daemon['command'], 'octane:start')
+        );
+        if (! $daemon) {
+            return;
+        }
+
+        $port = 8000;
+        Shell::exec("fuser -n tcp -k {$port}");
+        $this->info("Kill processes consuming port {$port}");
+
+        sleep(3);
+
+        $ploi->api("servers/{$server['id']}/daemons/{$daemon['id']}/restart", method: 'POST');
+        $this->info('Restarted Laravel Octane daemon');
     }
 
     protected function isSwooleServerRunning(): bool
