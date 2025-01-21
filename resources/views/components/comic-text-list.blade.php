00f3226271856146c9a26bc4b7521eaad6ea3ee9
Eric Chow <yateric@gmail.com>
2025-01-21 09:13:53 +0000

    update date format

--- resources/views/components/comic-text-list.blade.php
+++ resources/views/components/comic-text-list.blade.php
@@ -8,7 +8,7 @@
                 <a href="{{ $comic->url() }}" class="hover:underline underline-offset-4" title="{{ $comic->name() }}">{{ $comic->name() }}</a>
             </div>
             <div class="flex-none text-sm text-zinc-500 dark:text-zinc-300 ml-2">
-                {{ $comic->last_updated_on->format(__('m-d')) }}
+                {{ $comic->last_updated_on->format(__('n-j')) }}
             </div>
         </div>
     @endforeach
